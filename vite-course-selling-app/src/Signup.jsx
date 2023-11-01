19d77bd235fc58911404e1c50956b4821a165ae8
root <root@Unaislaptop>
2023-11-01 14:41:32 +0530

    just created a scratch signup page

---
+++ vite-course-selling-app/src/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+        Username - <input type='text'></input>
+        <br />
+        Password - <input type='password'></input>
+        <br />
+        <button>Signup</button>
+    </div>
+  )
+}
+
+export default Signup
